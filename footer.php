144dc53bf008b37ffb565a33f3c350f152cdcf0f
Anisuzzaman Titas <titas@mazegeek.com>
2019-07-08 15:46:08 +0600

    header footer separate

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer id="site-footer" class="page-footer">
+
+    <div class="footer-left">
+        <span class="opacity-75"><i class="icon ion-android-call"></i>+ 88 01723 987 605</span>
+        <span class="opacity-75"><i class="icon ion-email"></i>email: <a href="#" class="cf_email">info@anisuzzaman.me</a></span>
+    </div>
+
+    <div class="footer-right">
+        <p class="note">© 2019 Developed by
+            <a href="http://anisuzzaman.me">
+                <span class="marked"> Titas</span>
+            </a>
+        </p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <?php wp_head(); ?>
+</head>
+
+<body id="menu" class="body-page">
+<!--[if lt IE 8]>
+<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!--<div class="pageloader" id="pageloader">-->
+<!--    <div>-->
+<!--        <div class="icon ion-spin"></div>-->
+<!--    </div>-->
+<!--</div>-->
+
+<header class="page-header navbar page-header-alpha scrolled-white menu-right topmenu-right">
+    <button class="navbar-toggler site-menu-icon" id="navMenuIcon">
+            <span class="menu-icon menu-icon-random">
+                <span class="bars">
+                    <span class="bar1"></span>
+                    <span class="bar2"></span>
+                    <span class="bar3"></span>
+                </span>
+            </span>
+    </button>
+    <a class="navbar-brand" href="#home">
+            <span class="logo">
+            <h1 class="light-logo">TITAS</h1>
+                <!--                <img class="light-logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-white.png" alt="Logo">-->
+            </span>
+    </a>
+    <div class="all-menu-wrapper" id="navbarMenu">
+        <nav class="navbar-mainmenu">
+            <ul class="navbar-nav mr-auto mr-auto-my">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html#home">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="item.html">Item</a>
+                </li>
+            </ul>
+        </nav>
+        <nav class="navbar-sidebar ">
+            <ul class="social">
+                <li>
+                    <a href="#">
+                        <i class="icon fa fa-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="icon fa fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="icon fa fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
